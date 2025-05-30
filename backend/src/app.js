f16282ff4338abfc6425444c12876ef277674356
neennera <neennera@gmail.com>
2025-05-30 19:29:02 +0700

    init : backend

---
+++ backend/src/app.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+module.exports = app;
+
+app.use(express.json());
+
+// listen to port 300
+if (require.main === module) {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+app.get("/", (req, res) => {
+  res.send("Welcome to TicketLemon! Ya");
+});
